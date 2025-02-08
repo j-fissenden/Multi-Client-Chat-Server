e85c4c6fbf5319a6936c5af03ea3ea4a4974e837
j-fissenden <jack.e.fissenden@gmail.com>
2025-02-08 06:28:24 +0000

    Client Connections

--- ChatClient.py
+++ ChatClient.py
@@ -8,9 +8,21 @@ class ChatClient:
     def __init__(self, aes_key, host = '127.0.0.1', port  = 12345):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        try:
+            self.client_socket = ssl.wrap_socket(self.client_socket, certfile = 'client.crt', keyfile = 'client.key')
+            self.client_socket.connect((host, port))
+            print(f"Connected to {host}:{port}")
 
-        # Add GUI and connection SSL
-        pass
+        except ssl.SSLError as e:
+            print(f"SSL/TLS error {e}")
+            messagebox.showerror("Connection Error", f"SSL/TLS error: {e}")
+            exit(1)
+
+        except socket.error as e:
+            print(f"Socket connection error: {e}")
+            messagebox.showerror("Connection Error", f"Socket connection error: {e}")
+            exit(1)
+        
 
     def encrypt_msg(self, msg):
         return self.cipher.encrypt(msg.encoded())
