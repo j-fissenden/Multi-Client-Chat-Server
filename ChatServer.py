f30267baa4f8ba4719c37a10767bcdb4e75f2126
jf532 <jf532@kent.ac.uk>
2025-02-08 05:39:30 +0000

    Syntax update

--- ChatServer.py
+++ ChatServer.py
@@ -4,7 +4,7 @@ import ssl
 from cryptography.fernet import Fernet
 
 class ChatServer:
-    def __init__(self, aes_key, host='0.0.0.0', port=12345):
+    def __init__(self, aes_key, host = '0.0.0.0', port = 12345):
         self.clients = {}  
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
